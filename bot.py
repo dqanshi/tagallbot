170dd79416179323f115efdd531d637a78663687
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2021-10-30 10:05:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,73 +45,107 @@ bot_token = os.environ.get("TOKEN")
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 spam_chats = []
 
-
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-  await event.reply(K, 
-                    buttons=(
-                      [Button.url('🧞‍♂️ owner', 'https://t.me/am_dq_fan'),   
-                      Button.url('♻️ add me', 'http://t.me/Miss_mention_bot?startgroup=true')]
-                    ),
-                    link_preview=False
-                   )
+  await event.reply(
+     K,
+    link_preview=False,
+    buttons=(
+      [
+        Button.url('🧞‍♂️OWNER', 'https://t.me/am_dq_fan'),
+        Button.url('♻️ add me', 'http://t.me/Miss_mention_bot?startgroup=true')
+      ]
+    )
+  )
+
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  await event.reply(helptext,
-                    buttons=(
-                      [Button.url('🧞‍♂️ owner', 'https://t.me/am_dq_fan'),
-                      Button.url('📦 Source', 'https://t.me/DQ_fanz')]
-                    ),
-                    link_preview=False
-                   )
-  
+  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github"
+  await event.reply(
+    helptext,
+    link_preview=False,
+    buttons=(
+      [
+        Button.url('🧞‍♂️OWNER', 'https://t.me/am_dq_fan'),
+        Button.url('source', 'https://t.me/danger_bots')
+      ]
+    )
+  )
+
 @client.on(events.NewMessage(pattern="^/all ?(.*)"))
-async def all(event):
+async def mentionall(event):
+  chat_id = event.chat_id
   if event.is_private:
     return await event.respond("__This command can be use in groups and channels!__")
   
-  admins = []
-  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
-    admins.append(admin.id)
-  if not event.sender_id in admins:
+  is_admin = False
+  try:
+    partici_ = await client(GetParticipantRequest(
+      event.chat_id,
+      event.sender_id
+    ))
+  except UserNotParticipantError:
+    is_admin = False
+  else:
+    if (
+      isinstance(
+        partici_.participant,
+        (
+          ChannelParticipantAdmin,
+          ChannelParticipantCreator
+        )
+      )
+    ):
+      is_admin = True
+  if not is_admin:
     return await event.respond("__Only admins can mention all!__")
   
-  if event.pattern_match.group(1):
+  if event.pattern_match.group(1) and event.is_reply:
+    return await event.respond("__Give me one argument!__")
+  elif event.pattern_match.group(1):
     mode = "text_on_cmd"
     msg = event.pattern_match.group(1)
-  elif event.reply_to_msg_id:
+  elif event.is_reply:
     mode = "text_on_reply"
-    msg = event.reply_to_msg_id
+    msg = await event.get_reply_message()
     if msg == None:
         return await event.respond("__I can't mention members for older messages! (messages which are sent before I'm added to group)__")
-  elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await event.respond("__Give me one argument!__")
   else:
     return await event.respond("__Reply to a message or give me some text to mention others!__")
   
-  if mode == "text_on_cmd":
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
-      usrnum += 1
-      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-      if usrnum == 5:
-        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
-        await asyncio.sleep(2)
-        usrnum = 0
-        usrtxt = ""
-        
-  if mode == "text_on_reply":
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
-      usrnum += 1
-      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-      if usrnum == 5:
-        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
-        await asyncio.sleep(2)
-        usrnum = 0
-        usrtxt = ""
-        
+  spam_chats.append(chat_id)
+  usrnum = 0
+  usrtxt = ''
+  async for usr in client.iter_participants(chat_id):
+    if not chat_id in spam_chats:
+      break
+    usrnum += 1
+    usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+    if usrnum == 5:
+      if mode == "text_on_cmd":
+        txt = f"{usrtxt}\n\n{msg}"
+        await client.send_message(chat_id, txt)
+      elif mode == "text_on_reply":
+        await msg.reply(usrtxt)
+      await asyncio.sleep(2)
+      usrnum = 0
+      usrtxt = ''
+  try:
+    spam_chats.remove(chat_id)
+  except:
+    pass
+
+@client.on(events.NewMessage(pattern="^/cancel$"))
+async def cancel_spam(event):
+  if not event.chat_id in spam_chats:
+    return await event.respond('__There is no proccess on going...__')
+  else:
+    try:
+      spam_chats.remove(event.chat_id)
+    except:
+      pass
+    return await event.respond('__Stopped.__')
+
 print(">> BOT STARTED <<")
 client.run_until_disconnected()
+
